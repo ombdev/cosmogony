57390a350d97315963dc9f71716d3daf3d1c11c2
Edwin Plauchu <j4nusx@yahoo.com>
2020-09-12 20:14:24 -0500

    COS-000: basic steps to originate a bill
    Signed-off-by: Edwin Plauchu <j4nusx@yahoo.com>

---
+++ DOS/billing/engine/do.py
@@ -0,0 +1,20 @@
+
+def _get_res_dir_paths():
+    """Obtain paths of the resource directories"""
+    pass
+
+def _get_issuer():
+    """Disects data of the bill issuer"""
+    pass
+
+def _pac_sign():
+    """Electronic sign of a PAC"""
+    pass
+
+def _store():
+    """Store at persistance mechanism the bill"""
+    pass
+
+def carry_out(bill_req):
+    """Incepts a cfdi bill"""
+    pass
